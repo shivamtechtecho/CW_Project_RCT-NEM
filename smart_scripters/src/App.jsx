92f3fc2ad7d05a1c27d773ecc969d01ddd8bae08
shivamtechtecho <shivamtechtecho@gmail.com>
2024-12-17 20:08:25 +0530

    First Commit Day 17

---
+++ smart_scripters/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Hotels } from './components/Hotels'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Hotels/>
+    </>
+  )
+}
+
+export default App
---
+++ smart_scripters/src/components/TravelPage.jsx
@@ -0,0 +1,7 @@
+export const TravelPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
